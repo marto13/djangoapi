94c3925b9a24d4ab3441126f1d1d9e82f9e9360c
Marto13 <m.sofroniev@abv.bg>
2026-04-22 19:19:08 +0300

    add db

--- app/app/tests.py
+++ app/app/tests.py
@@ -12,14 +12,10 @@ class CalcTests(SimpleTestCase):
 
     def  test_add_number(self):
         res = calc.add(5,6)
-
         self.assertEqual(res,11)
 
 
-
-
     def test_substract_numbers(self):
-
         res = calc.substract(10,15)
         self.assertEqual(res,5)
 
