58fb8a8ea07737b12660ae67c047a205e7455613
Marto13 <m.sofroniev@abv.bg>
2026-04-22 19:26:40 +0300

    add db

--- app/app/tests.py
+++ app/app/tests.py
@@ -9,7 +9,7 @@ from app import calc
 
 class CalcTests(SimpleTestCase):
 
-    def  test_add_number(self):
+    def test_add_number(self):
         res = calc.add(5, 6)
         self.assertEqual(res, 11)
 
