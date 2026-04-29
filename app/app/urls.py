a6cfce35f4f622c7614184fc0d19f7718cc6ab77
Marto13 <m.sofroniev@abv.bg>
2026-04-29 15:01:00 +0300

    manychanges

--- app/app/urls.py
+++ app/app/urls.py
@@ -20,6 +20,8 @@ from drf_spectacular.views import (
     SpectacularSwaggerView,
 )
 from django.http import HttpResponse
+from django.conf.urls.static import static
+from django.conf import settings
 
 
 def home(request):
@@ -46,3 +48,9 @@ urlpatterns = [
     path('api/user/', include('user.urls')),
     path('api/recipe/', include('recipe.urls')),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(
+        settings.MEDIA_URL,
+        document_root=settings.MEDIA_ROOT,
+    )
